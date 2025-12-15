7fe35495e7d5ac85fe846b903a00d77370c421a9
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2025-12-15 16:15:37 +0200

    TOURLY-2 - Defined navigation routes (Welcome, SignIn, SignUp)

---
+++ app/src/main/java/com/tourly/app/core/navigation/Route.kt
@@ -0,0 +1,17 @@
+package com.tourly.app.core.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface Route: NavKey {
+
+    @Serializable
+    data object Welcome: Route, NavKey
+
+    @Serializable
+    data object SignIn: Route, NavKey
+
+    @Serializable
+    data object SignUp: Route, NavKey
+}
