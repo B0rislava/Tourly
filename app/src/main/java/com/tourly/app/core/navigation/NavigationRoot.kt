56e721ae3b2b9f88b17cbecf694095268392d1d3
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2025-12-15 16:17:41 +0200

    TOURLY-2 - Implemented navigation for auth flow

---
+++ app/src/main/java/com/tourly/app/core/navigation/NavigationRoot.kt
@@ -0,0 +1,71 @@
+package com.tourly.app.core.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import androidx.savedstate.serialization.SavedStateConfiguration
+import com.tourly.app.login.presentation.ui.SignInScreen
+import com.tourly.app.login.presentation.ui.SignUpScreen
+import com.tourly.app.onboarding.presentation.ui.WelcomeScreen
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+
+@Composable
+fun NavigationRoot() {
+
+    val backStack = rememberNavBackStack(
+        configuration = SavedStateConfiguration {
+            serializersModule = SerializersModule {
+                polymorphic(NavKey::class) {
+                    subclass(Route.Welcome::class, Route.Welcome.serializer())
+                    subclass(Route.SignIn::class, Route.SignIn.serializer())
+                    subclass(Route.SignUp::class, Route.SignUp.serializer())
+                }
+            }
+        },
+        Route.Welcome
+    )
+    NavDisplay(
+        backStack = backStack,
+        entryProvider = { key ->
+            when(key) {
+                is Route.Welcome -> {
+                    NavEntry(key) {
+                        WelcomeScreen(
+                            onGetStartedClick = {
+                                backStack.add(Route.SignUp)
+                            }
+                        )
+                    }
+                }
+                is Route.SignIn -> {
+                    NavEntry(key) {
+                        SignInScreen (
+                            onNavigateToSignUp = {
+                                backStack.add(Route.SignUp)
+                            },
+                            onLoginSuccess = {
+                                // TODO: Navigate to Home
+                            }
+                        )
+                    }
+                }
+                is Route.SignUp -> {
+                    NavEntry(key) {
+                        SignUpScreen (
+                            onNavigateToSignIn = {
+                                backStack.add(Route.SignIn)
+                            },
+                            onSignUpSuccess = {
+                                // TODO: Navigate to Home
+                            }
+                        )
+                    }
+                }
+                else -> error("Unknown NavKey: $key")
+            }
+        }
+    )
+}
