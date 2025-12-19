9c12b0bec0078d0422c6e8c125565284513f703f
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2025-12-19 17:25:16 +0200

    TOURLY-2 - Updated the usage of colors

--- app/src/main/java/com/tourly/app/core/ui/components/foundation/PrimaryButton.kt
+++ app/src/main/java/com/tourly/app/core/ui/components/foundation/PrimaryButton.kt
@@ -25,9 +25,9 @@ fun PrimaryButton(
         shape = RoundedCornerShape(size = 12.dp),
         colors = ButtonDefaults.buttonColors(
             containerColor = MaterialTheme.colorScheme.primary,
-            contentColor = MaterialTheme.colorScheme.tertiary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
             disabledContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
-            disabledContentColor = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.5f)
+            disabledContentColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f)
         ),
         elevation = ButtonDefaults.buttonElevation(defaultElevation = 0.dp)
     ) {
