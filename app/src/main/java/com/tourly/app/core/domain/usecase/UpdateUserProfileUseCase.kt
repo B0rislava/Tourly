2729ca8a06836457c8688ad2b391e45e655b8daa
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2026-01-02 15:21:46 +0200

    TOURLY-8 - Implemented update profile use case logic

---
+++ app/src/main/java/com/tourly/app/core/domain/usecase/UpdateUserProfileUseCase.kt
@@ -0,0 +1,14 @@
+package com.tourly.app.core.domain.usecase
+
+import com.tourly.app.core.domain.repository.UserRepository
+import com.tourly.app.core.network.model.UpdateProfileRequestDto
+import com.tourly.app.core.network.model.UserDto
+import javax.inject.Inject
+
+class UpdateUserProfileUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) {
+    suspend operator fun invoke(token: String, request: UpdateProfileRequestDto): Result<UserDto> {
+        return userRepository.updateUserProfile(token, request)
+    }
+}
